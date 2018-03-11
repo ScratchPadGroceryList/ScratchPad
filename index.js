538a5583892818a56c771a7906c61eed7187fc0f
Kelton O'Brien <Keltonpobrien@gmail.com>
2018-03-11 15:18:25 -0500

    add postgre sql junk

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 // const
 const PORT = 8080;
 
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
 // require
 const io = require('socket.io')();
 
 // socket junk
 io.on('connection', function(socket) {
-  
+
+});
+
+//heroku pg junk
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
 });
