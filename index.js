907f9e99c015915f0e09a96d6f00d2540f7da9e5
turnipjs <shmincky1@gmail.com>
2018-03-11 19:04:36 -0500

    keygen function

--- index.js
+++ index.js
@@ -4,7 +4,23 @@ const PORT = 8080;
 // require
 const io = require('socket.io')();
 
+// non-application-specific helper functions.
+function makeKey(length = 10, type = "alphanum") {
+  let types = {
+    alpha: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+    alphal: "abcdefghijklmnopqrstuvwxyz",
+    alphau: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    alphanum: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
+    num: "0123456789" 
+  }
+  let key = "";
+  for (; length > 0; length--) {
+    key += types[type][Math.floor(Math.random() * types[type].length)]
+  }
+  return key;
+}
+
 // socket junk
 io.on('connection', function(socket) {
-  
+  socket.emit('ping', "it's working");
 });
